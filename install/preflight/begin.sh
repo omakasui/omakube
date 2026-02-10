229da5e84c31f12494d450f6f8cb2c182833f4f4
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-11 00:07:31 +0100

    Update logging path to use dynamic log file variable

--- install/preflight/begin.sh
+++ install/preflight/begin.sh
@@ -1,5 +1,5 @@
 clear_logo
 gum style --foreground 3 --padding "1 0 0 $PADDING_LEFT" "Installing $OMAKUB_BRAND..."
-gum style --foreground 4 "Logging to: /var/log/omakub-install.log"
+gum style --foreground 4 "Logging to: $OMAKUB_INSTALL_LOG_FILE"
 echo
 start_install_log
