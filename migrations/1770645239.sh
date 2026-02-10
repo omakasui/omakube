6683e6f665a0d84bbab7ef8c36fef584d40603a9
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-10 13:58:20 +0100

    Show echo only if needed

--- migrations/1770645239.sh
+++ migrations/1770645239.sh
@@ -1,8 +1,6 @@
-#! /bin/bash
-
-echo "Updating Spotify GPG key..."
 
 if omakub-pkg-present spotify-client; then
+  echo "Updating Spotify GPG key..."
   curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 fi
